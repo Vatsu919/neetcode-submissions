fac4c485af4b614dce06c132333ddd92052f046d
Vatshal Patel <vatsunp11@gmail.com>
2026-08-10 19:36:07 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void fun(vector<int> &nums, int ci, vector<int> &temp, set<vector<int>> &ans) {
+        if(ci>nums.size())return ;
+        ans.insert(temp);
+        if(ci==nums.size())return ;
+
+        temp.push_back(nums[ci]);
+        fun(nums,ci+1,temp,ans);
+        temp.pop_back();
+
+        fun(nums,ci+1,temp,ans);
+
+        
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        set<vector<int>> ans;
+        fun(nums,0,temp,ans);
+        return {ans.begin(),ans.end()};
+    }
+};
